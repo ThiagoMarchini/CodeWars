932a36354ff7ab8365e4c91aa1529ab74384b7a4
Thiago Marchini <thiago@msn.com>
2021-04-08 10:33:55 -0300

    more exercises 6kyu

--- javascript/6kyu/decodeMorse.js
+++ javascript/6kyu/decodeMorse.js
@@ -11,17 +11,61 @@
 
 const assert = require('assert');
 
-const decodeMorse = (morseCode) => {
+const decode = (morseCode) => {
+  if (morseCode === '') {return ''};
   const morseDict = {
-    '....': 'H',
-    '.': 'E',
-    '-.--': 'Y',
-    '.---': 'J',
-    '..-': 'U',
-    '-..': 'D'
+    '.-': 'a',
+    "-...": 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '-----': 0,
+    '.----': 1,
+    '..---': 2,
+    '...--': 3,
+    '....-': 4,
+    '.....': 5,
+    '-....': 6,
+    '--...': 7,
+    '---..': 8,
+    '----.': 9
   }
-  
+  const morse = morseCode.split('  ');
+  let n = 1;
+  let answer = '';
+  morse.forEach(element => {
+    const wordMorse = element.split(' ');
+    const word = wordMorse.map((letter) => {
+      answer += morseDict[letter];
+    })
+    if (morse.length > n) {
+      answer += ' ';
+      n += 1;
+    }
+  });
+  return answer;
 }
 
 
-assert.strictEqual(decodeMorse('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
+assert.strictEqual(decode(".... . .-.. .-.. ---  .-- --- .-. .-.. -.."), "hello world");
